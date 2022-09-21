3895ac685464a3d8741b03fa48f76739977e3be0
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-09-21 08:39:01 -0300

    Tercer ejercicio, tomar un array y devolverlo al reves

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let strAlReverso = ''
+    for (let i = str.length - 1; i >= 0; i--) {
+        strAlReverso += str[i]
+    }
+    return strAlReverso
 };
+
+console.log(reverseString('hola como estas?'))
 
 // Do not edit below this line
 module.exports = reverseString;
