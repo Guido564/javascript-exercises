c56068d02073d3d4e8f06ac9b96cd195b440de7f
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-09-21 10:05:15 -0300

    Sumar numeros de primero a ultimo uno por uno

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(primero, ultimo) {
+    let resultado = 0
+    if (primero < 0 || isNaN(primero) || isNaN(ultimo) || typeof ultimo === 'string' || typeof primero === 'string') {
+        return 'ERROR'
+    } else if (ultimo < primero){
+        for (let i = ultimo; i <= primero; i++){
+            resultado += i
+        }
+    } else {
+        for (let i = primero; i <= ultimo; i++){
+            resultado += i
+        }
+    }
+    
+    return resultado
 };
-
+console.log(sumAll(1, 4))
 // Do not edit below this line
 module.exports = sumAll;
