019b6c96a09a5a30b8d9750dd4cbbf7293e69f2e
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-09-21 08:17:57 -0300

    Primeros dos ejercicios, hola mundo y repeticion de strings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num >= 0) {
+        let repeticion = ''
+        for (let i = 0; i < num; i++){
+            repeticion += str
+        }
+        return repeticion
+    } else {
+        return 'ERROR'
+    }
+    
 };
-
+console.log(repeatString('hey', -19))
 // Do not edit below this line
 module.exports = repeatString;
