629a4f506488ac6143befad6376b4b711c677f45
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-09-21 10:39:22 -0300

    Calculo de anio bisiesto

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(anio) {
+    if(anio % 4 == 0 && anio % 100 != 0){
+        return true
+    } else if (anio % 100 == 0 && anio % 400 == 0) {
+        return true
+    } else {
+        return false
+    }
 };
 
+console.log(leapYears(700))
+
+leapYears(2000) // is a leap year: returns true
+leapYears(1985)
 // Do not edit below this line
 module.exports = leapYears;
