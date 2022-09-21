62d673b4d86e191e19f3c46c188a4af1d187e5ca
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-09-21 09:37:18 -0300

    Eliminar elementos de un array, 1 o multiples o 0

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...elementos) {
+    let arrayLimpio = []
+    let aEliminar = [...elementos]
+    for (let i = 0; i < arr.length; i++){
+        for (let j = 0; j < aEliminar.length; j++){
+            if (arr[i] === aEliminar[j]) {
+                delete arr[i]
+            }
+        }
+        if (arr[i] != undefined){
+            arrayLimpio.push(arr[i])
+        }
+    }
+    return arrayLimpio
 };
 
+console.log(removeFromArray([1, 2, 3, 4], 2))
 // Do not edit below this line
 module.exports = removeFromArray;
