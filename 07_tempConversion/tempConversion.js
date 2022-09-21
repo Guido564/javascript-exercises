2daa590ee351839b6030984ed24d81c6ecf3441d
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-09-21 10:56:35 -0300

    c a f y viceversa, conversion de temperaturas

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  c = (fahrenheit - 32) / 1.8
+    return Math.round(c * 10) / 10
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  f = celsius * 1.8 + 32
+  return Math.round(f * 10) / 10
 };
+
 
 // Do not edit below this line
 module.exports = {
