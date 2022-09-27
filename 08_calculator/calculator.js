1fcb5810235c4f74193e3e88e454ef86225e0f4c
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-09-27 18:51:18 -0300

    Actividades de calculadora

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = 0;
+  for (let i = 0; i < arr.length; i++){
+    total += arr[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = 1;
+  for (let i = 0; i < arr.length; i++){
+    total *= arr[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total
+    if (num == 0 || num == 1) {
+      total = 1;
+    } else {
+      total = num * factorial(num - 1)
+    }
+    return total
 };
 
 // Do not edit below this line
