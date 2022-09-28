948a0efc4d2af35b6e1cff0d3f0f1eafec1973be
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-09-28 07:22:20 -0300

    Capicua

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let strAlReverso = ''
+    for (let i = str.length - 1; i >= 0; i--) {
+        strAlReverso += str[i]
+    }
+    let comparacion1 = str.toLowerCase().split(' ').join('').split(',').join('').split(',').join('').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+    let comparacion2 = strAlReverso.toLowerCase().split(' ').join('').split(',').join('').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
 
+    return comparacion1 == comparacion2
 };
 
 // Do not edit below this line
