26ef25940df6c94b0d51d822d5f64f0283080d5a
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-09-28 08:15:36 -0300

    Find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        if (arr[i].yearOfDeath == undefined) {
+            arr[i].yearOfDeath = 2022
+        }
+    }
 
+    const oldest = arr.sort(function(a, b) {
+        const anterior = a.yearOfDeath - a.yearOfBirth
+        const posterior = b.yearOfDeath - b.yearOfBirth
+        return posterior - anterior;
+    })
+    return oldest[0]
 };
 
 // Do not edit below this line
