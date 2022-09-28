a630acf75947f8962eb751994025179eddb21887
Guido Pruzzo <guidopruzzo564@gmail.com>
2022-09-28 07:40:41 -0300

    Fibonacci and Get The Titles

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -4,7 +4,7 @@ const fibonacci = function(num) {
     } else {
         if (num == 0) {
             return 0;
-        } else if (num === 1) {
+        } else if (num == 1) {
             return 1;
         } else {
             return fibonacci(num - 1) + fibonacci(num - 2);
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bs) {
+    const titles = bs.map(function(book) {
+        return book.title;
+    })
+    return titles
 };
 
 // Do not edit below this line
